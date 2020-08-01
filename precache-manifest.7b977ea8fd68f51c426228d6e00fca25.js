cbeae5aa549f16ad254211ddaaa748255724bc8b
Maria Gusova <masha.gu@gmail.com>
2020-08-01 15:57:57 +0100

    Updates

---
+++ precache-manifest.7b977ea8fd68f51c426228d6e00fca25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805bd04d319239b4092c5da6ed07345d",
+    "url": "/mariagus/grumpygit.git/index.html"
+  },
+  {
+    "revision": "9f231b702630112aa114",
+    "url": "/mariagus/grumpygit.git/static/css/main.5b10db3d.chunk.css"
+  },
+  {
+    "revision": "8e8b64662f3cb091d245",
+    "url": "/mariagus/grumpygit.git/static/js/2.6a08d554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mariagus/grumpygit.git/static/js/2.6a08d554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f231b702630112aa114",
+    "url": "/mariagus/grumpygit.git/static/js/main.d608319a.chunk.js"
+  },
+  {
+    "revision": "8a7126356d47845ea2af",
+    "url": "/mariagus/grumpygit.git/static/js/runtime-main.82ca576c.js"
+  },
+  {
+    "revision": "adf06b6cf8729395897bc4d7182fa771",
+    "url": "/mariagus/grumpygit.git/static/media/grump.adf06b6c.png"
+  },
+  {
+    "revision": "8c202a4fa8bea0fd785be447af6e86e8",
+    "url": "/mariagus/grumpygit.git/static/media/square.8c202a4f.png"
+  }
+]);
