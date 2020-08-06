12d4131adc4f2e1aa9e969744d4b30598fdf61d3
Maria Gusova <masha.gu@gmail.com>
2020-08-06 12:31:18 +0200

    add reset button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import grumpy from "./assets/grump.png";
 import speechbubble from "./assets/square.png";
 import "./App.css";
@@ -18,12 +18,25 @@ function App() {
 function GrumpyMan() {
   const [initPhrase, setInitPhrase] = useState(true);
   const [rotate, setRotate] = useState(0);
-  const [quote, setQuote] = useState("");
+  const [displayedQuote, setDisplayedQuote] = useState("");
+  const [count, setCount] = useState(0);
+  const [reset, setReset] = useState(false);
+
+  useEffect(() => {
+    if (count > randomArr.length) {
+      setReset(true);
+    }
+  }, [count]);
 
   function handleClick() {
     setInitPhrase(false);
     setRotate(1);
-    setQuote(quoteArr[Math.floor(Math.random() * quoteArr.length)]);
+    setCount(count + 1);
+    setDisplayedQuote(randomArr[count]);
+  }
+
+  function resetGame() {
+    window.location.reload(false);
   }
 
   return (
@@ -32,7 +45,12 @@ function GrumpyMan() {
         <img id="bubble" src={speechbubble} alt="quotebubble" />
         <h2>
           {initPhrase ? "click my face to ruin your day" : ""}
-          {quote}
+          {displayedQuote}
+          {reset ? (
+            <button className="button" onClick={resetGame}>
+              "RESET"
+            </button>
+          ) : null}
         </h2>
         <img
           src={grumpy}
@@ -46,12 +64,14 @@ function GrumpyMan() {
     </div>
   );
 }
+
 const quoteArr = [
   "Just keep telling yourself you're special, like the other seven billion",
   "Keep chasing your dreams, even if it means making yourself and everyone around you miserable",
   "Nobody cares, so stop worrying! Geez, so self-absorbed!",
-  "There, there! You're not a loser. You're just not much of a winner.",
+  "There, there! You're not a loser. You're just not much of a winner either.",
   "Sorry, I wasn't listening. You told that story already",
+  "That's right, waste more time looking at this stupid page. Your time is worthless anyway.",
 ];
-
+let randomArr = quoteArr.sort(() => Math.random() - 0.5);
 export default App;
