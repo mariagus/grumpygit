c75c3fcc8a85208347994eb7d82fe68408d91a35
Maria Gusova <masha.gu@gmail.com>
2020-08-10 11:19:54 +0200

    Create separate component for quote area

---
+++ src/components/QuoteGenerator.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import speechbubble from "../assets/square.png";
+import grumpy from "../assets/grump.png";
+import "./QuoteGenerator.css";
+
+function QuoteGenerator() {
+  const [initPhrase, setInitPhrase] = useState(true);
+  const [rotate, setRotate] = useState(0);
+  const [displayedQuote, setDisplayedQuote] = useState("");
+  const [count, setCount] = useState(0);
+  const [reset, setReset] = useState(false);
+
+  useEffect(() => {
+    if (count > randomArr.length) {
+      setReset(true);
+    }
+  }, [count]);
+
+  function handleClick() {
+    setInitPhrase(false);
+    setRotate(1);
+    setCount(count + 1);
+    setDisplayedQuote(randomArr[count]);
+  }
+
+  function resetGame() {
+    window.location.reload(false);
+  }
+
+  return (
+    <div className="main">
+      <div className="speechBubbleWrapper">
+        <img id="bubble" src={speechbubble} alt="quotebubble" />
+        <h2>
+          {initPhrase ? "click my face to ruin your day" : ""}
+          {displayedQuote}
+          {reset ? (
+            <button className="button" onClick={resetGame}>
+              "RESET"
+            </button>
+          ) : null}
+        </h2>
+        <img
+          src={grumpy}
+          onClick={handleClick}
+          onAnimationEnd={() => setRotate(0)}
+          rotate={rotate}
+          className="grumpy"
+          alt="grumpyMan"
+        />
+      </div>
+    </div>
+  );
+}
+const quoteArr = [
+  "Just keep telling yourself you're special, like the other seven billion",
+  "Keep chasing your dreams, even if it means making yourself and everyone around you miserable",
+  "Nobody cares, so stop worrying! Geez, so self-absorbed!",
+  "There, there! You're not a loser. You're just not much of a winner either.",
+  "Sorry, I wasn't listening. You told that story already",
+  "That's right, waste more time looking at this stupid page. Your time is worthless anyway.",
+];
+let randomArr = quoteArr.sort(() => Math.random() - 0.5);
+
+export default QuoteGenerator;
